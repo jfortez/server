3d68660c62adf5a5f13e7532d1933278294b4231
jeanmcm <jeanmcm@hotmail.com>
2021-08-26 19:44:45 -0500

    configs

--- src/models/ConfigQueries.js
+++ src/models/ConfigQueries.js
@@ -1,6 +1,6 @@
 exports.getValues = () => {
-  return "SELECT * from config_values";
+  return "SELECT * from config_values ORDER BY id ASC LIMIT 1";
 };
 exports.updateValues = () => {
-  return "UPDATE config_values SET ? where id=1";
+  return "UPDATE config_values SET ? ORDER BY id ASC LIMIT 1";
 };
--- src/models/DetalleCompQueries.js
+++ src/models/DetalleCompQueries.js
@@ -2,8 +2,9 @@ exports.getDetalleCompras = () => {
   return "SELECT dc.id AS idDetCompra, dc.id_compra, c.id_proveedor, pv.ruc, pv.nombre AS nombre_proveedor,c.num_factura, c.fecha, dc.id_producto, p.cod_producto, p.nombre AS nombre_producto, dc.cantidad, dc.precio, dc.total, c.cantidad AS total_cantidad, c.total AS total_compra FROM detalle_compra dc, productos p, compras c, proveedores pv WHERE dc.id_compra=c.id AND dc.id_producto=p.id AND c.id_proveedor=pv.id";
 };
 exports.getDetalleComprasbyIdCompra = () => {
-  return "SELECT dc.id AS idDetCompra, dc.id_compra, c.id_proveedor, pv.ruc, pv.nombre AS nombre_proveedor,c.num_factura, c.fecha, dc.id_producto, p.cod_producto, p.nombre AS nombre_producto, dc.cantidad, dc.precio, dc.total, c.cantidad AS total_cantidad, c.total AS total_compra FROM detalle_compra dc, productos p, compras c, proveedores pv WHERE dc.id_compra=c.id AND dc.id_producto=p.id AND c.id_proveedor=pv.id AND id_compra=?";
+  return "SELECT dc.id AS idDetCompra, dc.id_compra, c.id_proveedor, pv.ruc, pv.nombre AS nombre_proveedor, pv.direccion AS dir_proveedor, pv.telefono AS tel_proveedor, pv.email AS email_proveedor,c.num_factura, c.fecha, dc.id_producto, p.cod_producto, p.nombre AS nombre_producto, dc.cantidad, dc.precio, dc.total, c.cantidad AS total_cantidad, c.total AS total_compra FROM detalle_compra dc, productos p, compras c, proveedores pv WHERE dc.id_compra=c.id AND dc.id_producto=p.id AND c.id_proveedor=pv.id AND id_compra=?";
 };
 exports.insertDetalleCompras = () => {
   return "INSERT INTO detalle_compra (id_compra, id_producto, cantidad, precio, total) VALUES ?";
 };
+//
